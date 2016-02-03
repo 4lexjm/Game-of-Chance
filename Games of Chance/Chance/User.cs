299ba52e19371e339ab70b3b7be19d9d2ae3c445
Alexandre Lebrun <alexjm.lebrun@gmail.com>
2016-02-03 04:19:02 +0100

    initial commit

---
+++ Games of Chance/Chance/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chance
+{
+    public abstract class User
+    {
+        enum Gender
+        {
+            Man,
+            Woman
+        }
+
+        public string Name { get; set; }
+    }
+}
